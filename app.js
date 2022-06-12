29fa1a7b1e374f41ba3d35600e4abae70034bf61
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-06-12 12:13:45 +0100

    done

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ class Person {
     this.height = height;
     this.country = country;
   }
+  getData() {
+    return `my name is ${this.name}, i am ${this.height}ft tall and i am from ${this.country}`;
+  }
 }
+
+const Ben = new Person('biggKodes', 6.3, 'Nigeria');
+// console.log(Ben.getData());
+const h1 = document.querySelector('h1');
+h1.innerText = Ben.getData();
